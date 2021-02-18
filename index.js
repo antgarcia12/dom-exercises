7d021195e2e3379aeed16d47c2a3acae4113fcd5
antgarcia12 <antgarcia014@gmail.com>
2021-02-18 16:44:16 -0700

    added loop to js file

--- index.js
+++ index.js
@@ -26,19 +26,21 @@ header2.innerText = 'Subheader';
 
 // 6. Remove the h1 tag. What happens? Why?
  /*
-There is an Uncaught Reference Error. The h1 is not defined. */
+The header disappears. It is removed and is no longer part of the document. */
+
+// newElement.removeChild(header1);
 
 // 7. After removing it, try appending the same h1 tag back to the body. What returns to the page? Why?
-/*
-The error still occurs because the h1 element is still undefined. */
+
+// body.appendChild(header1);
+
+/* its attached to the body, but it's not in the div we made, it comes after.*/
 
 // 8. Using a loop, add five new p tags to the body of the document. Each p tag should have different innerText based on its index e.g. "This is p tag 1, "This is p tag 2", ... , "This is p tag 5". 
 
 
-let paragraphs = document.createElement('p');
-
-for (let i = 5; i < paragraphs.length; i++) {
-    
+for (let i = 1; i <= 5; i++) {
+    let paragraphs = document.createElement('p');
+    paragraphs.innerHTML = `This is a p tag ${i}`;
+    body.appendChild(paragraphs);
 }
-
-console.log(paragraphs);
